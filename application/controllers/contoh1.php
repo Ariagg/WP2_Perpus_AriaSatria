3f09607974c67499690a0d72d12d120986484d25
7k <ariasatria8@gmail.com>
2022-10-17 21:49:25 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,8 @@
+<?php
+class contoh1 extends CI_controller
+	{
+	    public function contoh1()
+	{
+		echo"<h1>perkenalkan nama saya Aria</h1>";
+	}
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Aria</li>
+ <li>Nama Belakang : Satria</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Gandaria No. 168 jagakarsa</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ <li>Renang</li>
+ </ul>
+</section>
